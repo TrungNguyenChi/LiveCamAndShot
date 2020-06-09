a45efa5d4f51f1eae25ccbc455749bf20e354198
TrungNguyenChi <nguyenchitrung140397@gmail.com>
2020-06-09 11:02:09 +0700

    first commit

---
+++ LiveCamAndShot/LiveCamAndShot/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace LiveCamAndShot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Snapshoot = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(552, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(385, 361);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(409, 171);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Snapshoot
+            // 
+            this.btn_Snapshoot.Location = new System.Drawing.Point(409, 238);
+            this.btn_Snapshoot.Name = "btn_Snapshoot";
+            this.btn_Snapshoot.Size = new System.Drawing.Size(103, 29);
+            this.btn_Snapshoot.TabIndex = 3;
+            this.btn_Snapshoot.Text = "Snapshoot";
+            this.btn_Snapshoot.UseVisualStyleBackColor = true;
+            this.btn_Snapshoot.Click += new System.EventHandler(this.btn_Snapshoot_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(409, 105);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(12, 23);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(372, 361);
+            this.videoSourcePlayer1.TabIndex = 5;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 502);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_Snapshoot);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Snapshoot;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
